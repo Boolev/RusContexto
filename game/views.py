474a68458d167ca713dbeb1c9178b3650939e5c3
Daniel-tg <daniilb.official@gmail.com>
2022-11-29 23:31:54 +0500

    working_proto

--- game/views.py
+++ game/views.py
@@ -15,13 +15,14 @@ with open('game/game_files/words4guess.pickle', 'rb') as handle:
 
 guess_words_length = len(words4guess)
 
+user_guesses = []
+
+random_index = randrange(guess_words_length)
+secret_word = words4guess[random_index]
+
 
 def index(request):
     context = {}
-
-    random_index = randrange(guess_words_length)
-    secret_word = words4guess[random_index]
-
     context['secret_word'] = secret_word
 
     if request.method == 'POST':
@@ -31,12 +32,15 @@ def index(request):
         for pair in similarity_matrix[secret_word]:
             if input_word in pair:
                 context['pair_for_asked'] = pair
+                user_guesses.append(pair)
                 found = True
 
         if not found:
             messages.warning(request, 'Неизвестное слово')
 
-        top_5 = similarity_matrix[secret_word][:5]
-        context['top_5'] = top_5
+        #top_5 = similarity_matrix[secret_word][:5]
+        #context['top_5'] = top_5
+
+        context['user_guesses'] = sorted(user_guesses, key=lambda x: x[1])
         
     return render(request, 'game/index.html', context)
